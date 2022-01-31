d6a8191a3649ae3ff9ab97abe2189216bf25891a
calixte12 <calixtedeles@gmail.com>
2022-01-31 17:01:17 +0100

    new map

--- map.js
+++ map.js
@@ -1,3 +1,13 @@
+(function(name,data){
+ if(typeof onTileMapLoaded === 'undefined') {
+  if(typeof TileMaps === 'undefined') TileMaps = {};
+  TileMaps[name] = data;
+ } else {
+  onTileMapLoaded(name,data);
+ }
+ if(typeof module === 'object' && module && module.exports) {
+  module.exports = data;
+ }})("map",
 { "compressionlevel":-1,
  "height":21,
  "infinite":false,
@@ -381,4 +391,4 @@
  "type":"map",
  "version":"1.6",
  "width":31
-}
+});
